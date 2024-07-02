95d9ea203981731468832f22c19e06156dd19c5a
Qiu Kuining <qiukuining@163.com>
2024-07-02 18:11:12 +0800

    简化了模型处理部分

--- org.zgjzy.meta.component/src/main/java/org/zgjzy/meta/component/merge/package-info.java
+++
@@ -1,5 +0,0 @@
-// CHECKSTYLE_OFF: RegexpHeader
-/**
- * POM merger.
- */
-package org.zgjzy.meta.component.merge;
--- org.zgjzy.meta.model/src/main/java/org/zgjzy/meta/model/package-info.java
+++ org.zgjzy.meta.model/src/main/java/org/zgjzy/meta/model/package-info.java
@@ -1,6 +1,5 @@
-// CHECKSTYLE_OFF: RegexpHeader
 /**
- * Maven POM (Project Object Model) classes, generated from <code>maven.mdo</code> model.
- * The root class is {@link org.apache.maven.model.Model}.
+ * 中国建筑业协会标准《建筑信息模型数字成果元数据标准》，模型元数据核心类目录，其类定义在 <code>model-meta.mdo</code> 中定义，并由工具自动生成。
+ * 核心类是{@link org.zgjzy.meta.model.Model}.
  */
 package org.zgjzy.meta.model;
